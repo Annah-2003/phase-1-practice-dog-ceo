1a3d41e602d7a2192241653d31bb5cdc4a49b538
Irene Gitau <irene.gitau@strathmore.edu>
2023-10-03 23:09:12 +0300

    done lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"  
+const breedList = document.getElementById("breedList");
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+// Get all <li> elements within the <ul>
+const breedItems = breedList.getElementsByTagName("li");
+const newColor = "red";
+
+// Loop through each <li> and add a click event listener
+for (let i = 0; i < breedItems.length; i++) {
+    breedItems[i].addEventListener("click", function () {
+        this.style.color = newColor;
+        for (let j = 0; j < breedItems.length; j++) {
+            if (j !== i) {
+                breedItems[j].style.color = ""; // default color
+            }
+        }
+    });
+}
+fetch(imgUrl)
